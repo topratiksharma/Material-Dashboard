5f99a697718b5e9727ee6f2fe376b9c722b3537e
Pratik Sharma <pratiksharma@live.in>
2022-01-25 23:32:45 +0530

    Updated with Angular 13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { routing } from './app.routes';
-import { MatButtonModule, MatRadioModule, MatInputModule, MatMenuModule, MatCheckboxModule } from '@angular/material';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+// import { HttpModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { routing } from "./app.routes";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatInputModule } from "@angular/material/input";
+import { MatMenuModule } from "@angular/material/menu";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HomeComponent } from "./dashboard/home/home.component";
+import { ProfileComponent } from "./dashboard/profile/profile.component";
+import "hammerjs";
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { FigurecardComponent } from "./shared/figurecard/figurecard.component";
+import { ImagecardComponent } from "./shared/imagecard/imagecard.component";
+import { TableComponent } from "./dashboard/table/table.component";
+import { NotificationComponent } from "./dashboard/notification/notification.component";
+import { MsgIconBtnComponent } from "./shared/msgiconbtn/msgiconbtn.component";
+import { SweetAlertComponent } from "./dashboard/sweetalert/sweetalert.component";
+import { LoginComponent } from "./page/login/login.component";
+import { RootComponent } from "./dashboard/root/root.component";
+import { RegisterComponent } from "./page/register/register.component";
+import { LockComponent } from "./page/lock/lock.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
+import { SettingsComponent } from "./dashboard/settings/settings.component";
+import { PriceTableComponent } from "./dashboard/component/pricetable/pricetable.component";
+import { PanelsComponent } from "./dashboard/component/panels/panels.component";
 
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './dashboard/home/home.component';
-import { ProfileComponent } from './dashboard/profile/profile.component';
-import 'hammerjs';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FigurecardComponent } from './shared/figurecard/figurecard.component';
-import { ImagecardComponent } from './shared/imagecard/imagecard.component';
-import { TableComponent } from './dashboard/table/table.component';
-import { NotificationComponent } from './dashboard/notification/notification.component';
-import { MsgIconBtnComponent } from './shared/msgiconbtn/msgiconbtn.component';
-import { SweetAlertComponent } from './dashboard/sweetalert/sweetalert.component';
-import { LoginComponent } from './page/login/login.component';
-import { RootComponent } from './dashboard/root/root.component';
-import { RegisterComponent } from './page/register/register.component';
-import { LockComponent } from './page/lock/lock.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { SettingsComponent } from './dashboard/settings/settings.component';
-import { PriceTableComponent } from './dashboard/component/pricetable/pricetable.component';
-import { PanelsComponent } from './dashboard/component/panels/panels.component';
-
-import { SettingsService } from './services/settings.service';
-import { WizardComponent } from './dashboard/component/wizard/wizard.component';
+import { SettingsService } from "./services/settings.service";
+import { WizardComponent } from "./dashboard/component/wizard/wizard.component";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -53,21 +57,21 @@ import { WizardComponent } from './dashboard/component/wizard/wizard.component';
     SettingsComponent,
     PriceTableComponent,
     PanelsComponent,
-    WizardComponent
+    WizardComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
     routing,
     BrowserAnimationsModule,
     MatButtonModule,
     MatRadioModule,
     MatInputModule,
     MatMenuModule,
-    MatCheckboxModule
+    CommonModule,
+    MatCheckboxModule,
   ],
   providers: [SettingsService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
